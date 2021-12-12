d11b36fcb6c2b65a136c3803f30861f758c32bd6
Vladimir Blinov <zemetrans21@yandex.ru>
2021-12-12 22:59:30 +0300

    sprint-1/step-2 feat: Добавить хедер и таб в BurgerIngredients

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import styles from './BurgerIngredients.module.css'
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 
 export default function BurgerIngredients() {
-  return <p>BurgerIngredients</p>
+  const [current, setCurrent] = React.useState('Булки')
+  return(
+    <section className={`${styles.burgerIngredients} pt-10`}>
+      <h2 className={`${styles.ingredientsHeader} text text_type_main-large mb-10`}>Соберите бургер</h2>
+      <div style={{ display: 'flex' }}>
+        <Tab value='Булки>' active={current === 'Булки'} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value='Соусы' active={current === 'Соусы'} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab value='Начинки' active={current === 'Начинки'} onClick={setCurrent}>
+          Начинки
+        </Tab>
+    </div>
+    </section>
+  )
 }
