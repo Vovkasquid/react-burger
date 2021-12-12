e1cb3a115deda1af8f9906da711bec61e73f334a
Владимир Блинов <zemetrans@192.168.1.7>
2021-12-12 16:33:29 +0300

    main feat: инициализация репозитория

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: { 'prettier/prettier': 0,  "import/no-named-as-default": 0, 'import/no-named-as-default-member': 0 , "react/prop-types": 0, 'react/jsx-props-no-spreading': 0, 'react/jsx-no-bind' : 0 },
+  
+}
