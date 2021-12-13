f7df328c6cd3cc3f535811248f868b0f33c9abe1
Vladimir Blinov <zemetrans21@yandex.ru>
2021-12-13 21:33:54 +0300

    sprint-1/step-2: сверстать основые компонента конструктора бургеров

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from './BurgerConstructor.module.css'
 import BurgerConstructorItem from '../BurgerConstructorItem/BurgerConstructorItem'
 import data from '../../utils/data'
+import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 
 export default function BurgerConstructor() {
   return (
     <section className={`${styles.burgerConstructorContainer} pt-25 pl-4 pr-4`}>
-      <ul className={`${styles.burgerConstructorList}`}>
+      <ul className={`${styles.burgerConstructorList} mb-10`}>
           {data.map((item, index) => {
             return (
               <li key={index}>
@@ -15,6 +16,12 @@ export default function BurgerConstructor() {
             )
           })}
       </ul>
+      <div className={`${styles.burgerConstructorPriceContainer} mt-10`}>
+        <p className={`${styles.burgerConstructorPrice} text text_type_main-large`}>625<CurrencyIcon type="primary" /></p>
+        <Button type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
     </section>
   )
 }
