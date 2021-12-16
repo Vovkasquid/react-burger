abbbedc1b23f2ebbe634a194c584af385683dc67
Vladimir Blinov <zemetrans21@yandex.ru>
2021-12-16 23:20:02 +0300

    sprint-1/step-2 feat: добавить запрос к апи

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,28 @@ import styles from './App.module.css'
 import AppHeader from '../AppHeader/AppHeader'
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor'
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients'
+import { BURGER_API_URL } from '../../utils/constants'
+
+ // Метод для проверки ответа
+ function checkResponse(res : any) {
+  if (res.ok) {
+    return res.json();
+  }
+  // Если условие не выполнено, то делаем промис с ошибкой
+  return Promise.reject(res);
+}
 
 function App() {
+  const [burgerData, setBurgerData] = React.useState([])
+
+  React.useEffect(() => {
+    fetch(BURGER_API_URL).then((response) => checkResponse(response))
+    .then((data) => {
+      console.log(data)
+      setBurgerData(data)
+    })
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className={styles.app}>
       <AppHeader />
