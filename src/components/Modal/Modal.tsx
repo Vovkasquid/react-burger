2047738f36ec045a0a25c475b7f7194703dbc000
Vladimir Blinov <zemetrans21@yandex.ru>
2021-12-18 14:05:04 +0300

    sprint-1/step-2: feat: добавить скрытие попапа по дефолту

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,13 +3,13 @@ import styles from './Modal.module.css'
 import ModalOverley from '../ModalOverley/ModalOverley'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
-const Modal = () => {
+const Modal = ( { image } : any ) => {
   return (
     <ModalOverley>
       <div className={`${styles.modal} pt-10 pr-10 pb-15 pl-10`}>
-        <div className={styles.modalHeader}>
-          <h3 className={`text text_type_main-large`}>Детали ингридиета</h3>
-          <CloseIcon type="primary" />
+        <div className={styles.modalHeaderContainer}>
+          <h3 className={`${styles.modalHeader} text text_type_main-large`}>Детали ингредиента</h3>
+          <button className={styles.closeButton}><CloseIcon type="primary" /></button>
         </div>
       </div>
     </ModalOverley>
