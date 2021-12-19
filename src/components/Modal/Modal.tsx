e7027c090442bc05952f87ac54faeb3520b80635
Vladimir Blinov <zemetrans21@yandex.ru>
2021-12-19 18:28:34 +0300

    sprint-1/step-2 feat: доверстать попап завершения заказа

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -15,6 +15,8 @@ const Modal = ( { isModalVisible, closePopup, ingredient, isOrder } : any ) => {
         <p className="text text_type_digits-large mb-8">034536</p>
         <p className={`text text_type_main-medium mb-15`}>идентификатор заказа</p>
         <img src={succesIcon} className={`${styles.successImage} mb-15`} alt="изображение картинки успешного заказа" />
+        <p className="text text_type_main-default mb-2">Ваш заказ начали готовить</p>
+        <p className="text text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
 
       </div>
       :
