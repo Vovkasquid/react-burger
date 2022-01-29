12c97ff1cc9e686e9334fbc78c2b3f9638143fc2
Vladimir Blinov <zemetrans21@yandex.ru>
2022-01-29 23:41:02 +0300

    sprint-2/step-2 feat: сделать заготовки редьюсеров и подключить их в корневой редьюсер

---
+++ src/services/reducers/reducers.js
@@ -0,0 +1,34 @@
+const initialReceivedComponents = {
+  buns: [],
+  sauces: [],
+  mainIngrediets: [],
+}
+
+const initialCurrentIngredients = {
+  currentBun: [],
+  currentIngredients: [],
+}
+
+const initialDetailIngredient = {
+  ingredient: [],
+}
+
+const initialOrder = {
+  order: [],
+}
+
+export const receivedComponentsReducer = (state = initialReceivedComponents, action) => {
+  return 0
+}
+
+export const currentIngredientsReducer = (state = initialCurrentIngredients, action) => {
+  return 0
+}
+
+export const detailIngredientReducer = (state = initialDetailIngredient, action) => {
+  return 0
+}
+
+export const orderReducer = (state = initialOrder, action) => {
+  return 0
+}
--- src/services/reducers/rootReducer.js
+++ src/services/reducers/rootReducer.js
@@ -1,7 +1,16 @@
 import { combineReducers } from 'redux'
+import {
+  receivedComponentsReducer,
+  currentIngredientsReducer,
+  detailIngredientReducer,
+  orderReducer
+} from './reducers'
 
 const rootReducer = combineReducers({
- 
+  receivedComponents: receivedComponentsReducer,
+  currentIngredients: currentIngredientsReducer,
+  detailIngredient: detailIngredientReducer,
+  order: orderReducer,
 })
 
 export default rootReducer
