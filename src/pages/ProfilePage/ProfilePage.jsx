98f1f81263b1bc0ca15c2bc9fa5458fd2b0842fa
Vladimir Blinov <zemetrans21@yandex.ru>
2022-04-02 13:19:36 +0300

    sprint-3/step-1 feat: отверстать менюшку в профиле

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import styles from './ProfilePage.module'
-import AppHeader from "../../components/AppHeader/AppHeader";
+import { Link } from 'react-router-dom'
+import styles from './ProfilePage.module.css'
+import AppHeader from '../../components/AppHeader/AppHeader'
+
 
 const ProfilePage = () => {
   return (
-    <main>
+    <div className={styles.profilePage}>
       <AppHeader />
-
-    </main>
+      <main className={styles.profileContainer}>
+        <ul className={styles.linkContainer}>
+          <li className={styles.listItem}><p className='text text_type_main-medium'>Профиль</p></li>
+          <li className={styles.listItem}><Link className={`${styles.link} text text_type_main-medium`} to="/profile/orders">История заказов</Link></li>
+          <li className={styles.listItem}><Link className={`${styles.link} text text_type_main-medium`} to="/login">Выход</Link></li>
+          <li className={`${styles.listItem} mt-20`} style={{width: '320px'}}><p className='text_type_main-default text_color_inactive' style={{margin: 0}} >В этом разделе вы можете изменить свои персональные данные</p></li>
+        </ul>
+      </main>
+    </div>
   )
 }
 
