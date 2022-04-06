6123147b8a9951470dea79452e75061b2537ce94
Vladimir Blinov <zemetrans21@yandex.ru>
2022-04-06 20:03:07 +0300

    sprint-3/step-1 fix: пофиксить защищённый маршрут

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,27 +1,17 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUser } from "../../services/actions/user";
+import { useSelector } from 'react-redux'
+import { getCookie } from '../../utils/coockies'
 
 export function ProtectedRoute({ children, ...rest }) {
-  const dispatch = useDispatch()
   const userState = useSelector(store => store.user)
-
-  const init = () => {
-    dispatch(getUser())
-  }
-
-  React.useEffect(() => {
-    init()
-  }, [])
-
   return (
     <Route
       {...rest}
       // Получим текущий маршрут, с которого произойдёт переадресация
       // для неавторизованного пользователя
       render={({ location }) =>
-        userState.name ? (
+        getCookie('token') || userState?.name ? (
           children
         ) : (
           <Redirect
