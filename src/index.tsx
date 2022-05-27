0018168ef6a1113dadeb722534f7e9a80842b8a6
Vladimir Blinov <zemetrans21@yandex.ru>
2022-05-27 22:04:13 +0300

    sprint-5/step-1 feat: типизировать index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './components/App/App.tsx'
+import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
 import { compose, createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './services/reducers/rootReducer'
 import thunk from 'redux-thunk'
+import { TBurgerConstructorsItemsReducer } from './services/reducers/burgerConstructorIngredientsReducer'
+import { TDetailIngredientReducers } from './services/reducers/detailIngredientReducer'
+import { TOrderReducer } from './services/reducers/orderReducer'
+import { TReceivedComponentsReducer } from './services/reducers/receivedComponentsReducer'
+import { TResetAndForgotPasswordReducer } from './services/reducers/resetAndForgotPassword'
+import { TUserReducer } from './services/reducers/userReducer'
+
+type TApplicationActions = TBurgerConstructorsItemsReducer |
+  TDetailIngredientReducers |
+  TOrderReducer |
+  TReceivedComponentsReducer |
+  TResetAndForgotPasswordReducer |
+  TUserReducer
+
+export type RootState = ReturnType<typeof store.getState>
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 // Настраиваем девтулзу
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    // @ts-ignore
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose
 
